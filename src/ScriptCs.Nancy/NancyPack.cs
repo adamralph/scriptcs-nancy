c4ab992281f5f641fbf9e469ab9f4c30ea87b79b
Adam Ralph <adam@adamralph.com>
2013-05-09 19:03:02 +0200

    removed redundant moduleTypes params on NancyPack.Host() overloads re-ordered parameters on NancyPack.Host() overloads to match NancyHost constructors added System.Uri overloads to NancyPack.Host() incremented to 0.2.0

--- src/ScriptCs.Nancy/NancyPack.cs
+++ src/ScriptCs.Nancy/NancyPack.cs
@@ -12,6 +12,7 @@ namespace ScriptCs.Nancy
 
     public class NancyPack : IScriptPackContext
     {
+        // quickstart
         public void Host(params Type[] moduleTypes)
         {
             this.Host(8888, moduleTypes);
@@ -22,43 +23,65 @@ namespace ScriptCs.Nancy
             this.Host(string.Concat("http://localhost:", port.ToString(CultureInfo.InvariantCulture), "/"), moduleTypes);
         }
 
+        // URI strings
         public void Host(string baseUriString, params Type[] moduleTypes)
         {
-            this.Host(new Bootstrapper(moduleTypes), baseUriString, moduleTypes);
-        }
-
-        [CLSCompliant(false)]
-        public void Host(INancyBootstrapper bootstrapper, string baseUriString, params Type[] moduleTypes)
-        {
-            using (var host = new NancyHost(bootstrapper, new Uri(baseUriString)))
-            {
-                Run(baseUriString, host);
-            }
+            this.Host(new Uri(baseUriString), moduleTypes);
         }
 
         [CLSCompliant(false)]
         public void Host(HostConfiguration hostConfiguration, string baseUriString, params Type[] moduleTypes)
         {
-            using (var host = new NancyHost(new Bootstrapper(moduleTypes), hostConfiguration, new Uri(baseUriString)))
+            this.Host(hostConfiguration, new Uri(baseUriString), moduleTypes);
+        }
+
+        [CLSCompliant(false)]
+        public void Host(INancyBootstrapper bootstrapper, string baseUriString)
+        {
+            this.Host(bootstrapper, new Uri(baseUriString));
+        }
+
+        [CLSCompliant(false)]
+        public void Host(INancyBootstrapper bootstrapper, HostConfiguration hostConfiguration, string baseUriString)
+        {
+            this.Host(bootstrapper, hostConfiguration, new Uri(baseUriString));
+        }
+
+        // main
+        public void Host(Uri baseUri, params Type[] moduleTypes)
+        {
+            this.Host(new Bootstrapper(moduleTypes), baseUri);
+        }
+
+        [CLSCompliant(false)]
+        public void Host(HostConfiguration hostConfiguration, Uri baseUri, params Type[] moduleTypes)
+        {
+            this.Host(new Bootstrapper(moduleTypes), hostConfiguration, baseUri);
+        }
+
+        [CLSCompliant(false)]
+        public void Host(INancyBootstrapper bootstrapper, Uri baseUri)
+        {
+            using (var host = new NancyHost(bootstrapper, baseUri))
             {
-                Run(baseUriString, host);
+                Run(baseUri, host);
             }
         }
 
         [CLSCompliant(false)]
-        public void Host(HostConfiguration hostConfiguration, INancyBootstrapper bootstrapper, string baseUriString, params Type[] moduleTypes)
+        public void Host(INancyBootstrapper bootstrapper, HostConfiguration hostConfiguration, Uri baseUri)
         {
-            using (var host = new NancyHost(bootstrapper, hostConfiguration, new Uri(baseUriString)))
+            using (var host = new NancyHost(bootstrapper, hostConfiguration, baseUri))
             {
-                Run(baseUriString, host);
+                Run(baseUri, host);
             }
         }
 
-        private static void Run(string baseUriString, NancyHost host)
+        private static void Run(Uri baseUri, NancyHost host)
         {
             host.Start();
 
-            Console.WriteLine("Nancy is running at " + baseUriString);
+            Console.WriteLine("Nancy is running at " + baseUri.ToString());
             Console.WriteLine("Press any key to end");
             Console.ReadKey();
 
